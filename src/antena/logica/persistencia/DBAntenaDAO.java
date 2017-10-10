c6ea7342000b1f93bcfd95eaec793fde5d1f1654
Douglas <douglas.vasco79@gmail.com>
2017-10-09 23:38:00 -0300

    Alterado e Configurado mas sem os codigos

--- src/antena/logica/persistencia/DBAntenaDAO.java
+++ src/antena/logica/persistencia/DBAntenaDAO.java
@@ -1,5 +1,27 @@
 package antena.logica.persistencia;
 
-public class DBAntenaDAO {
+import java.util.Set;
+
+import antena.logica.dominio.Antena;
+
+public class DBAntenaDAO extends GenericDAO implements AntenaDAOIF {
+
+	@Override
+	public Set<Antena> buscarTodas() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizar(Antena antena) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Antena buscarPeloId(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/antena/logica/persistencia/DBCelularDAO.java
+++ src/antena/logica/persistencia/DBCelularDAO.java
@@ -1,5 +1,19 @@
 package antena.logica.persistencia;
 
-public class DBCelularDAO {
+import antena.logica.dominio.Celular;
+
+public class DBCelularDAO extends GenericDAO implements CelularDAOIF {
+
+	@Override
+	public Celular buscarPeloId(String id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizar(Celular celular) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
